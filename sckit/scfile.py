c34cab4b00d70ccfbcf4b577d5a5acc0a5a9cd0c
shawn.chen <kertansul@gmail.com>
2017-10-25 03:31:59 +0000

    init draft
    - add scfile create and remove folder effectively

---
+++ sckit/scfile.py
@@ -0,0 +1,28 @@
+import os
+
+
+cl_for_mkdir = set()
+def mkdir_if_missing( dir_pth ):
+    '''
+    create directory for the specified path, pass if it already exists or being checked
+    '''
+    global cl_for_mkdir
+    if dir_pth in cl_for_mkdir:
+        pass
+    else:
+        if not os.path.exists( dir_pth ):
+            os.makedirs( dir_pth )
+        cl_for_mkdir.add( dir_pth )
+
+cl_for_rmdir = set()
+def rmdir_if_exists( dir_pth ):
+    '''
+    remove directory for the specified path, pass if it already exists or being checked
+    '''
+    global cl_for_rmdir
+    if dir_path in cl_for_rmdir:
+        pass
+    else:
+        if os.path.exists( dir_pth ):
+            os.removedirs( dir_pth )
+        cl_for_rmdir.add( dir_pth )
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='sckit',
+    version='0.1',
+    packages=find_packages(),
+    author='Shawn Chen',
+    author_email='shawn.chen@viscovery.com',
+    zip_safe=False,
+)
